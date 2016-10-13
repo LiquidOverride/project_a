fcd5b5c2c8983d9ff9fbc30cedc371a8c8d23ee0
RoelandOostdam <Roeland.Oostdam@outlook.com>
2016-10-14 00:48:35 +0200

    mv.bat fixed link

--- anthrax_launcher/mv.bat
+++ anthrax_launcher/mv.bat
@@ -1,2 +1,2 @@
-xcopy /s /z "crimsonlauncher.pyw" "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup"
-xcopy /s /z "crimsonlauncher2.pyw" "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup"
+xcopy /s /z "anthrax_launcher.pyw" "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup"
+xcopy /s /z "anthrax_launcher2.pyw" "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup"
